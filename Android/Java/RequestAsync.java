3bc00a73142e2caf3e52b2abc2af5831f1e06d04
PatelOmar <54734501+PatelOmar@users.noreply.github.com>
2020-01-06 23:56:30 -0500

    Add files via upload

---
+++ Android/Java/RequestAsync.java
@@ -0,0 +1,15 @@
+package com.example.chatjelly;
+
+import android.os.AsyncTask;
+
+public class RequestAsync extends AsyncTask<String,String,String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
